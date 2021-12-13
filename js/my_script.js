9a15aaa9ee190b24e009648841b99cf051e994b5
clarken92 <hunken92@gmail.com>
2021-12-13 15:52:50 +1100

    Update 3

---
+++ js/my_script.js
@@ -0,0 +1,50 @@
+/* Ensure navbar is highlighted correctly */
+$(document).ready(function() {
+	var file_name = location.pathname.split("/").slice(-1)[0];
+	
+	console.log(file_name);
+	
+	var nav_items = document.getElementsByClassName("nav-item");
+	for (i=0; i < nav_items.length; i++){
+		// console.log(i);
+		// console.log(nav_items[i].nodeName);
+		// console.log(nav_items[i].firstChild.nodeName);
+		// console.log(nav_items[i].firstChild.getAttribute('href'));
+		
+		nav_link = nav_items[i].getElementsByTagName("a")[0];
+		
+		if (((file_name == "index.html" || file_name == "") && (nav_link.getAttribute("href") == "/")) || 
+			(file_name == nav_link.getAttribute("href"))){
+			if (!nav_items[i].className.includes("active")){
+				nav_items[i].className += " active";
+			}
+		}
+		else {
+			if (nav_items[i].className.includes("active")){
+				nav_items[i].className = nav_items[i].className.replace("active", "");
+			}
+		}
+		
+		nav_items[i].className = nav_items[i].className.replace(/\s+/g, ' ').trim();
+	}
+});
+
+
+function showOlderNews(obj){	
+	var recent_news = document.getElementById("recent-news")
+	var old_news = document.getElementById("old-news")
+	if ((recent_news != null) && (old_news != null)){
+		recent_news.style.display = 'none';
+		old_news.style.display = '';
+	}
+};
+
+
+function showLatestNews(obj){	
+	var recent_news = document.getElementById("recent-news")
+	var old_news = document.getElementById("old-news")
+	if ((recent_news != null) && (old_news != null)){
+		recent_news.style.display = '';
+		old_news.style.display = 'none';
+	}
+};
